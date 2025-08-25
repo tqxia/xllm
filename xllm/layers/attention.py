eecae321380ac9949e26f2eb578962dcc4ac2984
kikoxia <kikoxia@tencent.com>
2025-08-25 17:07:43 +0800

    Implement packed sequence batching with block-diagonal masking

--- xllm/layers/attention.py
+++ xllm/layers/attention.py
@@ -21,7 +21,7 @@ class AttentionWithoutKVCache(nn.Module):
         self.num_kv_heads = num_kv_heads
         self.num_queries_per_kv = num_heads // num_kv_heads
 
-    def forward(self, q: torch.Tensor, k: torch.Tensor, v: torch.Tensor):
+    def forwar_with_unbatched_loop(self, q: torch.Tensor, k: torch.Tensor, v: torch.Tensor):
         ctx = get_forward_context()
         cu_seqlens = ctx.cu_seqlens
         orig_q, orig_k, orig_v = q, k, v
@@ -34,16 +34,50 @@ class AttentionWithoutKVCache(nn.Module):
 
             cu_seqlen = cu_seqlens[idx+1] - cu_seqlens[idx]
             # print(f"Processing batch {idx}, cu_seqlen {cu_seqlen} total {len(cu_seqlens)-1}")
-            q = q.view(1, cu_seqlen, self.num_heads, self.head_dim)
-            k = k.view(1, cu_seqlen, self.num_kv_heads, self.head_dim)
-            v = v.view(1, cu_seqlen, self.num_kv_heads, self.head_dim)
+            q = q.view(cu_seqlen, self.num_heads, self.head_dim)
+            k = k.view(cu_seqlen, self.num_kv_heads, self.head_dim)
+            v = v.view(cu_seqlen, self.num_kv_heads, self.head_dim)
 
-            q, k, v = (x.transpose(1, 2) for x in (q, k, v))
+            q, k, v = (x.transpose(0, 1) for x in (q, k, v))
             o = F.scaled_dot_product_attention(q, k, v, is_causal=True, scale=self.scale, enable_gqa=True)
-            o = o.transpose(1, 2).squeeze(0).reshape(cu_seqlen, -1)
+            o = o.transpose(0, 1).reshape(cu_seqlen, -1)
             res.append(o)
         o = torch.cat(res, 0)
         return o
+
+    def forward_with_block_diagonal_mask(self, q: torch.Tensor, k: torch.Tensor, v: torch.Tensor):
+        ctx = get_forward_context()
+        cu_seqlens = ctx.cu_seqlens
+
+        # Manually build a block-diagonal causal mask
+        assert cu_seqlens[-1] == q.shape[0]
+        N = cu_seqlens[-1]
+        mask = torch.zeros((N, N), dtype=torch.bool, device=q.device)
+
+        for idx in range(len(cu_seqlens)-1):
+            # Create a causal mask for current segment
+            start = cu_seqlens[idx]
+            l = cu_seqlens[idx+1] - cu_seqlens[idx]
+            # reshape [l] to [l, 1]
+            rows = torch.arange(start, start + l, device=q.device)[:, None]
+            # reshape [l] to [1, l]
+            cols = torch.arange(start, start + l, device=q.device)[None, :]
+            # broadcast rows and compare with cols to create causal mask of [l, l]
+            causal_block = (rows >= cols)
+            mask[start:start + l, start:start + l] = causal_block
+
+        q = q.view(N, self.num_heads, self.head_dim)
+        k = k.view(N, self.num_kv_heads, self.head_dim)
+        v = v.view(N, self.num_kv_heads, self.head_dim)
+
+        q, k, v = (x.transpose(0, 1) for x in (q, k, v))
+        o = F.scaled_dot_product_attention(q, k, v, attn_mask=mask, scale=self.scale, enable_gqa=True)
+
+        o = o.transpose(0, 1).reshape(N, -1)
+        return o
+
+    def forward(self, q: torch.Tensor, k: torch.Tensor, v: torch.Tensor):
+        return self.forward_with_block_diagonal_mask(q, k, v)
 
 
 class Attention(nn.Module):
